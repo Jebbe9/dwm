f8f49d12a9af7aada89dd75bf642ccf7f4c5c411
Jebbe9 <jebbede@protonmail.com>
2020-12-30 13:13:33 +0200

    dwm

--- config.def.h
+++ config.def.h
@@ -1,22 +1,27 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 0;        /* 0 means no bar */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "adobe-source-code-pro:size=7" };
 static const char dmenufont[]       = "adobe-source-code-pro:size=5";
+/* background color */
 static const char col_gray1[]       = "#222222";
+/* inactive window border color */
 static const char col_gray2[]       = "#444444";
+/* font color */
 static const char col_gray3[]       = "#bbbbbb";
+/* current tag and current window font color */
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/* active window and top bar color */ 
+static const char col_cyan[]        = "#F47D03";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -69,21 +74,28 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+
+/* terminal */
 static const char *termcmd[]  = { "kitty", NULL };
+
+/* other apps*/
 static const char *firefoxcmd[] = { "firefox", NULL };
 static const char *discordcmd[] = { "discord", NULL };
 static const char *spotifycmd[] = { "spotify", NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
+
+/* media */
 static const char *mutecmd[]  = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 static const char *volupcmd[]  = { "pactl", "set-sink-volume", "0", "+5%", NULL };
 static const char *voldowncmd[]  = { "pactl", "set-sink-volume", "0", "-5%", NULL };
 static const char *playerctltoggle[] = { "playerctl", "play-pause", NULL };
 static const char *playerctlnextcmd[] = { "playerctl", "next", NULL };
 static const char *playerctlprevcmd[] = { "playerctl", "previous", NULL };
+
+/* unnecessary */
 static const char *htopcmd[] = { "kitty", "-e", "htop", NULL };
 static const char *rangercmd[] = { "kitty", "-e", "ranger", NULL };
 static const char *ttyclockcmd[] = { "kitty", "-e", "tty-clock", "-c", "-s", NULL };
-static const char *newsboatcmd[] = { "kitty", "-e", "newsboat", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -96,7 +108,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_h,      spawn,          {.v = htopcmd } },
 	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = rangercmd } },
 	{ MODKEY|ShiftMask,             XK_c,      spawn,          {.v = ttyclockcmd } },
-	{ MODKEY|ShiftMask,		XK_n,	   spawn,	   {.v = newsboatcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
