9cf61e348da7fc686ead11c5077da37e77498863
Jebbe9 <jebbede@protonmail.com>
2021-01-08 16:38:59 +0200

    dwm

--- config.h
+++ config.h
@@ -28,8 +28,9 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "" }; /*"", "", "", "" }; */
 
 static const Rule rules[] = {
 	/* xprop(1):
