b1ccc32944110ee4b2edd864c0b0b7f3e8803eb7
Jebbe9 <jebbede@protonmail.com>
2021-04-09 14:43:52 +0300

    build

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 30;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 30;        /* 0 means no bar */
-static const int topbar             = 30;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Term:size=14" };
-static const char dmenufont[]       = "Iosevka Term:size=14";
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 0;       /* snap pixel */
+static const unsigned int gappih    = 3;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 3;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 3;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 3;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Iosevka Term:size=18" };
+static const char dmenufont[]       = "Iosevka Term:size=18";
 
 static const char col_grayD1[]       = "#222222";
 static const char col_grayD2[]       = "#444444";
@@ -41,10 +41,17 @@ static const char col_pinkD[]	    = "#c68baa";
 //}; 
 
 // Purppura & musta
-static const char *colors[][3]      = {
-	/*              fg (text)  bg (behind text)  border   */
+ //static const char *colors[][3]      = {
+	 ///*              fg (text)  bg (behind text)  border   */
+	 //[SchemeNorm] = { col_grayB1, col_grayD1, col_black }, // Not selected
+	 //[SchemeSel]  = { col_grayB2, col_purple,  col_purple  }, // Selected
+ //};
+
+// Valkonen & musta
+static const char *colors[][3]	    = {
+	/*	        fg (text  bg  (behind text)  border   */	
 	[SchemeNorm] = { col_grayB1, col_grayD1, col_black }, // Not selected
-	[SchemeSel]  = { col_grayB2, col_purple,  col_purple  }, // Selected
+	[SchemeSel]  = { col_black, col_white,  col_white  }, // Selected
 };
 
 
--- config.h
+++ config.h
@@ -3,15 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 3;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 3;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 3;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 3;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Term:size=14" };
-static const char dmenufont[]       = "Iosevka Term:size=14";
+static const char *fonts[]          = { "Iosevka Term:size=18" };
+static const char dmenufont[]       = "Iosevka Term:size=18";
 
 static const char col_grayD1[]       = "#222222";
 static const char col_grayD2[]       = "#444444";
@@ -41,10 +41,17 @@ static const char col_pinkD[]	    = "#c68baa";
 //}; 
 
 // Purppura & musta
-static const char *colors[][3]      = {
-	/*              fg (text)  bg (behind text)  border   */
+ //static const char *colors[][3]      = {
+	 ///*              fg (text)  bg (behind text)  border   */
+	 //[SchemeNorm] = { col_grayB1, col_grayD1, col_black }, // Not selected
+	 //[SchemeSel]  = { col_grayB2, col_purple,  col_purple  }, // Selected
+ //};
+
+// Valkonen & musta
+static const char *colors[][3]	    = {
+	/*	        fg (text  bg  (behind text)  border   */	
 	[SchemeNorm] = { col_grayB1, col_grayD1, col_black }, // Not selected
-	[SchemeSel]  = { col_grayB2, col_purple,  col_purple  }, // Selected
+	[SchemeSel]  = { col_black, col_white,  col_white  }, // Selected
 };
 
 
@@ -107,7 +114,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_d,      spawn,          SHCMD("discord") },
 	{ MODKEY|ShiftMask,		XK_s,      spawn,          SHCMD("st -e mocp") },
 	{ MODKEY|ShiftMask,		XK_f,      spawn,          SHCMD("firefox") }, 
-	{ MODKEY|ShiftMask,		XK_p,	   spawn,          SHCMD("pavucontrol") },
         { 0,                            XK_Print,  spawn,          SHCMD("maim -s /home/jere/Pic/Scr/$(date +%F_%H:%M).png | xclip -selection clipboard -t image/png") },
 	{ MODKEY,			XK_Insert, spawn,	   SHCMD("killall slstatus && slstatus &") },
 
@@ -151,7 +157,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	
-	//Media keys
+	/* Media keys */
 	{ 0, XF86XK_AudioMute,		spawn, SHCMD("pactl set-sink-mute 0 toggle") },
 	{ 0, XF86XK_AudioLowerVolume,	spawn, SHCMD("pactl set-sink-volume 0 -5%") },
 	{ 0, XF86XK_AudioRaiseVolume,	spawn, SHCMD("pactl set-sink-volume 0 +5%") },
@@ -159,7 +165,7 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioPrev,		spawn, SHCMD("playerctl previous") },
 	{ 0, XF86XK_AudioNext, 		spawn, SHCMD("playerctl next") },
 	
-	//Moc
+	/* Moc */
 	{ 0, XF86XK_AudioPlay,		spawn, SHCMD("mocp -G") },
 	{ 0, XF86XK_AudioPrev,		spawn, SHCMD("mocp -r") },
 	{ 0, XF86XK_AudioNext,		spawn, SHCMD("mocp -f") },
