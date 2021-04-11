3831b6d472884abeec28a768e854e1cd0f514077
Jebbe9 <jebbede@protonmail.com>
2021-04-11 10:30:24 +0300

    build

--- config.def.h
+++ config.def.h
@@ -7,10 +7,10 @@ static const unsigned int gappih    = 20;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Term:size=14" };
+static const char *fonts[]          = { "Iosevka Term:size=12" };
 static const char dmenufont[]       = "Iosevka Term:size=14";
 
 static const char col_grayD1[]       = "#222222";
