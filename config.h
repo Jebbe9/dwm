a06bff925b2fd3e12b30ce2bb60113311697d81a
Jebbe9 <jebbede@protonmail.com>
2022-03-10 13:35:08 +0200

    master focus juttu

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 24;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 24;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 24;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 24;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -79,6 +79,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_Home,   spawn,          SHCMD("qbittorrent") },
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("rofi -show drun") },
 	{ MODKEY,			XK_f,      spawn,          SHCMD("firefox") }, 
+	{ MODKEY,			XK_s,	   spawn,	   SHCMD("st -e cmus") },
         { 0,                            XK_Print,  spawn,          SHCMD("flameshot gui") },
 	{ MODKEY,			XK_Insert, spawn,	   SHCMD("pkill slstatus && slstatus &") },
 
@@ -105,7 +106,6 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioPrev,		spawn, SHCMD("mpc prev") },
 	{ 0, XF86XK_AudioNext,		spawn, SHCMD("mpc next") },
 
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -113,6 +113,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+
+	// Gaps
 	{ MODKEY,                       XK_x,      incrgaps,       {.i = +5 } },
 	{ MODKEY,                       XK_z,      incrgaps,       {.i = -5 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
@@ -129,7 +131,9 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	
+
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
