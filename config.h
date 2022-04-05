427d9f6777e8342e5aa87503617e0f0ca66ceabd
Jebbe9 <jebbede@protonmail.com>
2022-04-05 03:36:09 +0300

    barpadding

--- config.h
+++ config.h
@@ -6,13 +6,15 @@ static const unsigned int snap      = 32;       /* snap pixel */
 
 /* Gapit oli 24 */
 static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "IBMPlexMono:size=13" };
 static const char dmenufont[]       = "Terminus:size=15";
 static const char col_gray1[]       = "#222222";
