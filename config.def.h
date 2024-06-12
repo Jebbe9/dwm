6415e452afc0f0bd639aee5faa8935c67a6bf6c8
Jebbe9 <jebbede@protonmail.com>
2024-06-12 10:53:58 +0300

    muutoksii

--- config.def.h
+++ config.def.h
@@ -5,23 +5,23 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 5;       /* snap pixel */
 
 /* Gapit oli 15 */
-static const unsigned int gappih    = 24;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 24;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 24;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 24;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
-static const char *fonts[]          = { "mono:size=13" };
+static const char *fonts[]          = { "FiraCode-Regular:size=13" };
 static const char dmenufont[]       = "Terminus:size=19";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#033664";
+static const char col_cyan[]        = "#663399";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
@@ -29,7 +29,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", };
+static const char *tags[] = { "", "", "", "", "", };
 
 static const Rule rules[] = {
 	/* xprop(1):
