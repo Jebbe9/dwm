1d9ef8b480394070ce29d5911ae9aeaedda783ab
Jebbe9 <jebbede@protonmail.com>
2024-11-11 03:03:55 +0200

    muutoksii

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 5;       /* snap pixel */
 
 /* Gapit oli 10 */
-static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 40;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 40;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 40;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 40;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
